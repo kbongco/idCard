083fccd85a4427eea2545584ad9f8a06b7490e84
Kathleen B <kathleenb@Kathleens-MacBook-Air.local>
2021-03-10 14:46:41 -0500

    need to figure out css grid

--- src/Card.jsx
+++ src/Card.jsx
@@ -5,11 +5,20 @@ export default function Card() {
   console.log(dummy);
   return (
     <>
-      <h1>Hi</h1>
       <div className="card">
-        <div className="info-container">
-          <img className="photo" src="{dummy.image}" />
-        </div>
+        {dummy.map((dummys) => (
+          <>
+            <div className="photo-container">
+              <img id="photo" src={dummys.image} />
+            </div>
+            <div className="info-container">
+              <h1 id="catchphrase">{dummys.catchphrase}</h1>
+              <p id="name">{dummys.name}</p>
+              <p id="occupation">{dummys.occupation}</p>
+              <p id="hobbies">{dummys.hobbies}</p>
+            </div>
+          </>
+        ))}
       </div>
     </>
   );
